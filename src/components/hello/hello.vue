beb8477ae15cf7b416fcb15efbbaed9c8afe72fc
skyworth <skyworth@skyworthdeiMac.local>
2018-01-31 11:51:17 +0800

    华工问答

---
+++ src/components/hello/hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+      <router-link to="/helloworld">hello!(click me)</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/helloworld/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="helloworld">
+    hello world!
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
